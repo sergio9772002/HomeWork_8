1a0fb0ea136c574c87f2aac82285e9b89c1c7f9e
Sergio <sergio977@rambler.ru>
2022-12-03 15:35:29 +0400

    add task54

---
+++ task54/Program.cs
@@ -0,0 +1,76 @@
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+int[,] Generate2DArray(int lengthRow, int lengthCol, int deviation)
+{
+    int[,] array = new int[lengthRow, lengthCol];
+    for (int i = 0; i < lengthRow; i++)
+    {
+        for (int j  = 0; j < lengthCol; j++)
+        {
+            array[i, j] = new Random().Next(- deviation, deviation + 1);
+        }
+    }
+    return array;
+}
+void printColor(string information, ConsoleColor color, bool newLine = false)
+{
+    Console.ForegroundColor = color;
+    Console.Write(information);
+    Console.ResetColor();
+    if(newLine)
+    {
+        Console.WriteLine();
+    }
+}
+void print2dArray(int[,] array, string Name = "")
+{
+    printColor(Name, ConsoleColor.DarkCyan, true);
+    Console.Write("\t");
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        printColor(i + "\t", ConsoleColor.DarkYellow,(i >= array.GetLength(1) - 1));
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printColor(i + "\t", ConsoleColor.DarkYellow);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] sortedArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int n = j + 1; n < array.GetLength(1); n++)
+            {
+                int buf;
+                if (array[i, j] < array[i, n])
+                {
+                    buf = array[i, j];
+                    array[i, j] = array[i, n];
+                    array[i, n] = buf;
+                }
+            }
+                
+        }
+    }
+    return array;
+}
+int[,] generatedArray = Generate2DArray(5, 5, 10);
+print2dArray(generatedArray, "Исходный массив");
+int[,] sortArray = sortedArray(generatedArray);
+print2dArray(generatedArray, "Развернутый массив");
