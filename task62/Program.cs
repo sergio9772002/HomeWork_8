8216cba03df30ecd708958ad6c7bea7a49c5ba7e
Sergio <sergio977@rambler.ru>
2022-12-04 17:34:29 +0400

    add task62

---
+++ task62/Program.cs
@@ -0,0 +1,62 @@
+﻿/*Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07
+*/
+
+int[,] arraySpiral(int lengthRow, int lengthCol)
+{
+    int[,] array = new int[lengthRow, lengthCol];
+    int count = 1;
+    int i = 0;
+    int j = 0;
+    while (count <= array.GetLength(0) * array.GetLength(1))
+    {
+        array[i, j] = count;
+        count++;
+       if (i <= j + 1 && i + j < array.GetLength(1) - 1) j++;
+       else 
+       {
+            if (i < j && i + j >= array.GetLength(0) - 1) i++;
+            else
+            {
+                if (i >= j && i + j > array.GetLength(1) - 1) j--;
+                else i--;
+            }
+       }
+    }
+    return array;
+}
+void printColor(string information, ConsoleColor color, bool newLine = false)
+{
+    Console.ForegroundColor = color;
+    Console.Write(information);
+    Console.ResetColor();
+    if(newLine)
+    {
+        Console.WriteLine();
+    }
+}
+void print2dArray(int[,] array, string Name = "")
+{
+    printColor(Name, ConsoleColor.DarkCyan, true);
+    Console.Write("\t");
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        printColor(i + "\t", ConsoleColor.DarkYellow,(i >= array.GetLength(1) - 1));
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printColor(i + "\t", ConsoleColor.DarkYellow);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] generatedArray = arraySpiral(4, 4);
+print2dArray(generatedArray, "Массив");
